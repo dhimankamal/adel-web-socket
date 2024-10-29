e76258a053571161b3b0062b02bf176d38a8bf33
kamal <dhimank079@gmail.com>
2024-10-29 12:13:41 +0530

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import '../app.css';
+
+	import Header from '$lib/components/Header.svelte';
+	import type { LayoutServerData } from './$types';
+
+	export let data: LayoutServerData;
+</script>
+
+<Header isAuthenticated={data.isAuthentication} user={data.userProfile} />
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { LayoutServerData } from './$types';
+
+	export let data: LayoutServerData;
+</script>
+
+<svelte:head>
+	<title>Kinde Auth kamal</title>
+</svelte:head>
+
+<main>
+	<div class="container mx-auto flex py-20 items-center justify-center">
+		<div class="">
+			<p class="text-center">Kinde authentication (email+code)</p>
+		</div>
+	</div>
+</main>
