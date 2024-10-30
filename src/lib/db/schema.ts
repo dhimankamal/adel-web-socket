8736886524df69fad8fe9422b85abc1db8d18a11
kamal <dhimank079@gmail.com>
2024-10-30 16:33:14 +0530

    user table add

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -5,3 +5,10 @@ export const myTable = pgTable("datatable", {
   userid: text("userid"),
   value: text("value"),
 });
+
+export const user = pgTable("user", {
+  id: serial("id").primaryKey(),
+  email: text("email").unique(),
+  name: text("name"),
+  password: text("password"),
+});
